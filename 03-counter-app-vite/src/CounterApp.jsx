5843babdd6825cfab3c95b7b690a9fb2ba535421
Raul Romero <link_2642@hotmail.com>
2022-11-26 10:22:08 -0600

    curso react

---
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const CounterApp = ({ value }) =>{
+    //se le pasas como paramtro el de las props, para usarlo, su valor inicial es el que mandamos desde el padre
+
+    const [ counter, setCounter ] = useState( value ) //use state crea la propiedad en las dev tool, el primer valor desestructurado es el nombre que  le queremos poner a ese valor, el segundo valor es el nombre de la funcion mediante la cual queremos mutar el valor
+                                                  // notese que en ningun momento se "definio" la funcion solo se usa y hace lo de dentro de los parentesis le dice a react, el estado cambio, redibuja el componente
+
+    const handleAdd = () => {
+        //console.log(event)
+        setCounter(counter+1)
+        //setCounter( (c) => c+1 ) es el mismo
+    }
+
+    const handleLess = () => {
+        setCounter(counter-1)
+    }
+
+    const handleReset = () => {
+        setCounter(value)//el mismo de las props
+    }
+
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h2> { counter } </h2>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleLess }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
+        </>
+    );
+
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired,
+}
+
+CounterApp.defaultProps = {
+    value: 0,
+}
---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Raúl'
+}
+
+const getMessage = (message) => {
+    return message.title;
+}
+
+export const FirstApp = ({ title, subtitle, name }) =>{
+
+    if(!title){
+        throw new Error('No existe el titulo')
+    }
+    //console.log(props);
+
+    return(
+        <>
+            <h1>{ title }</h1>
+            {/* <code>{ JSON.stringify(newMessage) } </code> */}
+            <p>{ subtitle }</p>
+            <p>{ name }</p>
+        </>
+    );
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    name: 'Raul Romero',
+    subtitle: 'No hay subtitulo',
+    title: 'No hay titulo',
+}
