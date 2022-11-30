e80bdfd2b1820d7710d4d16673be913ecbfa0770
Raul Romero <raul.romero26@gmail.com>
2022-11-29 21:42:00 -0600

    update: continuando con la seccion

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 // import React from 'react'
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['Programacion','Dragon Ball'])
 
-    const onAddCategory = () =>{
-        setCategories(['Valorant', ...categories]);
+    const onAddCategory = ( onNewCategory ) =>{
+        console.log(onNewCategory);
+        setCategories([ onNewCategory, ...categories ]);
         //setCategories(cat => [...cat,'Valorant'] );
     }
 
@@ -14,13 +16,17 @@ export const GifExpertApp = () => {
     <>
         <h1>GifExpertApp</h1>
 
+        <AddCategory 
+            //setCategories={ setCategories}{/*PROPIEDAD QUE HACE REFERENCIA A UNA FUNCION del padre se pasa al hijo*/}
+            onNewCategory = { event => onAddCategory(event) } //esto es una propiedad
+        /> 
         <ol>
             {
                 categories.map(category => {
                     return <li key={category}>{category}</li>
                 })
             }
-            <button onClick={onAddCategory}>Agregar</button>
+           
         </ol>
     </>
   )
---
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => { //desestructuracuib de las props
+
+  const [inputValue, setInputValue] = useState('')
+
+  const onInputChange = ({target}) => { //se dessestructura el event
+    setInputValue(target.value);
+   
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const newInputValue = inputValue.trim();
+    if(newInputValue.length<=1) return
+    //setCategories( categories => [inputValue,...categories] ); //al mandarlo en callback obtienes el "estado acutual" para no perder la data
+    onNewCategory(newInputValue)
+    setInputValue('') //para resetar
+  }
+
+  return (
+    <form onSubmit={(event)=> onSubmit(event)}>
+        <input 
+            type="text" 
+            placeholder="Buscar gifs" 
+            id="" 
+            value={ inputValue }
+            onChange={ (event) => onInputChange(event) } //se puede obviar el event
+        />
+    </form>
+  )
+}
