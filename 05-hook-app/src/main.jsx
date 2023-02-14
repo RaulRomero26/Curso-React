c35f7715156fe04f7838e2742371125620e7e157
RaulRomero26 <raul.romerod26@gmail.com>
2023-02-14 17:13:36 -0600

    update: previo al memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { Layout } from './05-useLayoutEffect/Layout';
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
+//import { FocusScreen } from './04-useRef/FocusScreen';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <FormWithCustomHook />
+    <Layout />
   //</React.StrictMode>,
 )
