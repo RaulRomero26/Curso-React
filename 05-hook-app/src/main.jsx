649385dc676289b9880a005e410d4346f5574671
RaulRomero26 <raul.romerod26@gmail.com>
2023-02-14 18:01:06 -0600

    update: useMemo hook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Layout } from './05-useLayoutEffect/Layout';
+import { MemoHook } from './06-memos/MemoHook';
+//import Memorize from './06-memos/Memorize';
+//import { Layout } from './05-useLayoutEffect/Layout';
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
 //import { FocusScreen } from './04-useRef/FocusScreen';
+
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Layout />
+    <MemoHook />
   //</React.StrictMode>,
 )
