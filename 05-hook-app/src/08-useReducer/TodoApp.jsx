655a90fc3ecebec0ff3031d65acb7bb94702045f
Raul Romero <raul.romerod26@gmail.com>
2023-02-19 11:37:46 -0600

    update: fin seccion 10

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,58 +1,27 @@
-import { useEffect, useReducer } from "react"
+
 import { TodoAdd } from "./components/TodoAdd"
 import { TodoList } from "./components/TodoList"
-import { todoReducer } from "./todoReducer"
-
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del alma',
-    //     done: false,
-    // },
-    // {
-    //     id: new Date().getTime() * 3,
-    //     description: 'Recolectar la piedra del poder',
-    //     done: false,
-    // },
-]
-
-const init = () =>{
-    return JSON.parse(localStorage.getItem('todos') ) || [];
-}
+import { useTodo } from "../hooks"
 
 export const TodoApp = () => {
 
-    const [ todos , dispatch] = useReducer( todoReducer, initialState, init);
+    //useTodo 
+    //exponer todos los handlers
+    const{ todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodo()
 
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos])
-    
-    //funciones de despacho del reducer
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-        dispatch( action );
-    }
-
-    const handleDeleteTodo = (id) => {
-        const action = {
-            type: '[TODO] Remove Todo',
-            payload: id
-        }
-        dispatch( action );
-    }
-
+  
     return (
     <>
-        <h1>Todo App: 10, <small>Pendientes: 2</small></h1>
+        <h1>Todo App: {todosCount}, <small>Pendientes: {pendingTodosCount}</small></h1>
         <hr />
 
         <div className="row">
             <div className="col-7">
-                <TodoList todos={todos} onDeleteTodo={handleDeleteTodo}/>
+                <TodoList 
+                    todos={todos} 
+                    onDeleteTodo={handleDeleteTodo}
+                    onToggleTodo={handleToggleTodo}
+                />
             </div>
             <div className="col-5">
                 
--- 05-hook-app/src/08-useReducer/components/TodoList.jsx
+++ 05-hook-app/src/08-useReducer/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import { TodoItem } from "./TodoItem"
 
-export const TodoList = ({todos = [], onDeleteTodo}) => {
+export const TodoList = ({todos = [], onDeleteTodo, onToggleTodo}) => {
 
 
 
@@ -8,7 +8,13 @@ export const TodoList = ({todos = [], onDeleteTodo}) => {
     <>
         <ul className="list-group">
             {
-                todos.map(todo => <TodoItem key={todo.id} todo={todo} onDeleteTodo={onDeleteTodo}/>)
+                todos.map(todo => 
+                  <TodoItem 
+                    key={todo.id} 
+                    todo={todo} 
+                    onDeleteTodo={onDeleteTodo}
+                    onToggleTodo={onToggleTodo}
+                    />)
             }
         </ul>
     </>
