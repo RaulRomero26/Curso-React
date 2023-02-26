2f22778b12a9e89025dd8a034a0956531490cbf5
Raul Romero <link_2642@hotmail.com>
2023-02-26 12:50:26 -0600

    update: pruebas en el authReducer

---
+++ 07-heroes-spa/test/auth/context/authReducer.test.js
@@ -0,0 +1,43 @@
+import { authReducer } from "../../../src/auth/context/authReducer"
+import { types } from "../../../src/auth/types/types";
+
+describe('Prueba en el autg Reducer', () => { 
+
+    test('debe de retornar el estado por defecto', () => { 
+        
+        const state = authReducer({logged:false}, {});
+        expect(state).toEqual({logged: false});
+
+     })
+
+     test('debe de (login) llamar al login , auntenticar y establecer el usuarii', () => { 
+
+        const action ={
+            type: types.login,
+            payload: {
+                name: 'Juan',
+                id: '123'
+            }
+        }
+
+        const state = authReducer({logged: false},action);
+        expect(state).toEqual({logged:true, user: action.payload})
+        
+      })
+
+      test('debe de (logout) borrar el nombre del usuario y logged en false', () => { 
+
+        const state = {
+            logged: true,
+            user: { id: '123', name: 'Juan'}
+        }
+        
+        const action = {
+            type: types.logout
+        }
+
+        const newState = authReducer(state, action);
+        expect(newState).toEqual({logged:false})
+        
+       })
+ })
