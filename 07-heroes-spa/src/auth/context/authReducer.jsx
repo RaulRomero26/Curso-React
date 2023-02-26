a213c0a79aa17f297134e93a03c2fda2a118add4
Raul Romero <link_2642@hotmail.com>
2023-02-25 19:36:58 -0600

    update: reducer, context y provider

---
+++ 07-heroes-spa/src/auth/context/authReducer.jsx
@@ -0,0 +1,27 @@
+import { types } from "../types/types";
+
+const initialState = {
+    logged: false,
+
+}
+
+export const authReducer = ( state = initialState, action ) => {
+    
+    switch (action.type) {
+
+        case types.login:
+             return{
+                ...state,//por si muta y necesitas tener mas propiedades en la sesion
+                logged: true,
+                name: action.payload
+             };
+
+        case types.logout:
+             return{
+                logged : false,
+             };
+    
+        default:
+            return state;
+    }
+}
---
+++ 07-heroes-spa/src/auth/types/types.js
@@ -0,0 +1,6 @@
+// son los tipos de acciones del dispatch del reducer
+
+export const types = {
+    login: '[Auth] Login',
+    logout: '[Auth] Logout',
+}
