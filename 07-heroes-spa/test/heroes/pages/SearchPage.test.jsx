99eb4804ade556c7a7668d28e8b2e1519a99f7b7
RaulRomero26 <link_2642@hotmail.com>
2023-02-28 17:23:57 -0600

    update: fin test searchpage

--- 07-heroes-spa/test/heroes/pages/SearchPage.test.jsx
+++ 07-heroes-spa/test/heroes/pages/SearchPage.test.jsx
@@ -1,9 +1,18 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom";
 import { SearchPage } from "../../../src/heroes/pages/SearchPage";
 
+const mockedUseNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUseNavigate
+}));
+
 
 describe('Pruebas en <SearchPage />', () => { 
+
+    beforeEach(() => jest.clearAllMocks());
 
     test('debe de mostrarse correctamente con valores por defecto', () => {
         
@@ -17,5 +26,54 @@ describe('Pruebas en <SearchPage />', () => {
 
     })
     
- 
+    
+    test('debe de mostrar a batman y el input con el valor del queryString', () => {
+        
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+        
+        const input = screen.getByRole('textbox');
+        expect(input.value).toBe('batman');
+        const img = screen.getByRole('img');
+        expect(img.src).toContain('/assets/heroes/dc-batman.jpg')
+
+        const aler = screen.getByLabelText('alert-danger');
+        expect(aler.style.display).toBe('none');
+
+    });
+    
+    test('debe de mostrar un error si no se encuentra el hero (batman123)', () => {
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const aler = screen.getByLabelText('alert-danger');
+        expect(aler.style.display).toBe('');
+
+    })
+
+    test('debe de llamar el navigate a la pantalla nueva', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/search']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input,{target: {name: 'searchText', value: 'superman'}})
+        
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(mockedUseNavigate).toHaveBeenCalledWith('?q=superman');
+
+    });
+
+
  })
