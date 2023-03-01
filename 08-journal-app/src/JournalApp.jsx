7a2969eeaabeff7b7400a519de782f7a867a3160
RaulRomero26 <link_2642@hotmail.com>
2023-03-01 13:15:22 -0600

    update: instalacion y config materialui

--- 08-journal-app/src/JournalApp.jsx
+++ 08-journal-app/src/JournalApp.jsx
@@ -1,11 +1,14 @@
 
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTheme"
 
 
 export const JournalApp = () => {
   return (
     <>  
-      <AppRouter/>
+      <AppTheme>
+        <AppRouter/>
+      </AppTheme>
     </>
   )
 }
