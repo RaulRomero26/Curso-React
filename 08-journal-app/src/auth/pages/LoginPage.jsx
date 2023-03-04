f619a927249ee1245496e50acb6b94f0ba3fd82c
Raul Romero <link_2642@hotmail.com>
2023-03-04 08:37:07 -0600

    update: auth layout, login,register

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -3,25 +3,13 @@ import { Link  as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid 
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
-    >
-      <Grid item
-      className='boxs-hadow'
-      xs={3}
-      sx={{ backgroundColor:'white', padding: 3, borderRadius: 2}}
-      >
-        <Typography variant='h5' sx={{ mb: 1 }}>
-          Login
-        </Typography>
+
+
+    <AuthLayout title='Login'>
 
         <form >
           <Grid container>
@@ -67,7 +55,8 @@ export const LoginPage = () => {
             </Link>
           </Grid>
         </form>
-      </Grid>
-    </Grid>
+
+    </AuthLayout>
+
   )
 }
