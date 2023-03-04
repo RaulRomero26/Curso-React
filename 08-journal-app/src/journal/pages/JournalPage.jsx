e6ef2fcbb585de64bd585c94fc4eed802a9d39f9
Raul Romero <link_2642@hotmail.com>
2023-03-04 11:08:38 -0600

    update: inicio del noteview

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,15 @@
 import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import React from 'react'
+import { NothingSelectedView,NoteView } from '../views'
 
 export const JournalPage = () => {
   return (
     <>
         <JournalLayout>
-          <Typography> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit assumenda amet repellat quo earum aperiam ullam, nemo repellendus repudiandae, dolor expedita. Veniam tenetur odit necessitatibus sequi quam, voluptatum voluptas quaerat. </Typography>
+          <NoteView/>
+          {/* <NothingSelectedView/> */}
+          {/* <Typography> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit assumenda amet repellat quo earum aperiam ullam, nemo repellendus repudiandae, dolor expedita. Veniam tenetur odit necessitatibus sequi quam, voluptatum voluptas quaerat. </Typography> */}
           {/* noting selected */}
           {/*notew view */}
         </JournalLayout>
