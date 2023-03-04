e4f75cd4b376d0b502f35fc692d9dcc9196e6803
Raul Romero <link_2642@hotmail.com>
2023-03-03 22:55:42 -0600

    update: inicio diseño login page

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,46 @@
+import { Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid 
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}
+    >
+      <Grid item
+      className='boxs-hadow'
+      xs={3}
+      sx={{ backgroundColor:'white', padding: 3, borderRadius: 2}}
+      >
+        <Typography variant='h5' sx={{ mb: 1 }}>
+          Login
+        </Typography>
+
+        <form >
+          <Grid container>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='correo@google.com'
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder='contraseña'
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
   )
 }
