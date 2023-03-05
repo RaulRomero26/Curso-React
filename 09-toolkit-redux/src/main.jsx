770373b2dc1fe5277cab5c46578ace71ed87e603
Raul Romero <link_2642@hotmail.com>
2023-03-05 11:57:19 -0600

    uptate: uso de rtk query

--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -7,11 +7,12 @@ import { Provider } from 'react-redux'
 // import App from './App'
 import { PokemonApp } from './PokemonApp'
 import './index.css'
+import { TodoApp } from './TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      <TodoApp/>
     </Provider>
   </React.StrictMode>,
 )
