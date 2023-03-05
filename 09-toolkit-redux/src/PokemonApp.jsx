47c4034838d3dcdf061dd87654f7d3ad29565ddc
Raul Romero <link_2642@hotmail.com>
2023-03-05 10:58:57 -0600

    update: axios

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "./store/slices/pokemon";
 
 
 export const PokemonApp = () => {
 
+    const { isLoading, pokemons, page } = useSelector( state => state.pokemons)
     const dispatch = useDispatch();
 
     useEffect( () => {
@@ -15,12 +16,21 @@ export const PokemonApp = () => {
     <>
         <h1>Pokemon app</h1>
         <hr />
-
+        <span>Loafing: { isLoading ? 'True' : 'False' }</span>
         <ul>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
+            {
+                pokemons.map( pokemon => {
+                    return ( <li key={pokemon.name}>{pokemon.name}</li>)
+                })
+            }
         </ul>
+
+        <button
+            disabled={ isLoading }
+            onClick={() => dispatch( getPokemons(page) )}
+        >
+            Next
+        </button>
     </>
   )
 }
