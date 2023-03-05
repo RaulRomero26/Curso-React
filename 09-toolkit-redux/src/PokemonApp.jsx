a76df3236afe21c4c6e51aaa2f4d079852fd92bb
Raul Romero <link_2642@hotmail.com>
2023-03-05 09:54:54 -0600

    update: thunks pokemon

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getPokemons } from "./store/slices/pokemon";
+
+
+export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch( getPokemons() );
+    }, [])
+
+  return (
+    <>
+        <h1>Pokemon app</h1>
+        <hr />
+
+        <ul>
+            <li>hola</li>
+            <li>hola</li>
+            <li>hola</li>
+        </ul>
+    </>
+  )
+}
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client'
 import { store } from './store'
 import { Provider } from 'react-redux'
 
-import App from './App'
+// import App from './App'
+import { PokemonApp } from './PokemonApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
