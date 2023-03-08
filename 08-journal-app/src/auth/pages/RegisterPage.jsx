78b6048f4056c8f2b4132bd23e13f9d0af07e260
RaulRomero26 <link_2642@hotmail.com>
2023-03-08 12:42:59 -0600

    update: errorMesagge user al ready

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 
 import { Link  as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Button, Grid, Link, TextField, Typography,Alert } from '@mui/material'
 
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
@@ -25,6 +25,9 @@ export const RegisterPage = () => {
 
   const dispatch = useDispatch()
   const [formSubmited, setFormSubmited] = useState(false)
+
+  const { status, errorMessage } = useSelector( state => state.auth );
+  const isCheckingAuthentication = useMemo( () => status === 'cheking',[status])
 
   const {
     formState, displayName, email,password, onInputChange, 
@@ -88,8 +91,15 @@ export const RegisterPage = () => {
             </Grid>
 
             <Grid container spacing={2} sx={{mb:2, mt:1,}}>
+              <Grid item xs={12}
+              display={!!errorMessage ? '':'none'}>
+                <Alert severity='error'>{errorMessage}</Alert>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx={{mb:2, mt:1,}}>
               <Grid item xs={12}>
                 <Button 
+                  disabled={isCheckingAuthentication}
                   type='submit'
                   variant='contained' 
                   fullWidth
