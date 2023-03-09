33f8042b7434f15d755af3ac8d74ae33b592cea7
RaulRomero26 <link_2642@hotmail.com>
2023-03-09 14:27:20 -0600

    update: inicio journalSlice

---
+++ 08-journal-app/src/store/journal/journalSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const journalSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = journalSlice.actions
+
+export default journalSlice.reducer
