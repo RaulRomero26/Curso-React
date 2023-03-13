097caa02166c6acd4c8fa3c098d0b608d2dc8d26
RaulRomero26 <link_2642@hotmail.com>
2023-03-13 11:21:17 -0600

    update: carga de las notas

--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
-import { addNewEmptyNote, setActiveNote,savingNewNote } from './journalSlice';
+import { loadNotes } from '../../helpers/loadNotes';
+import { addNewEmptyNote, setActiveNote,savingNewNote,setNotes } from './journalSlice';
 
 export const startNewNote = () => {
     return async (dispatch,getState) => {
@@ -23,7 +24,19 @@ export const startNewNote = () => {
         newNote.id = newDoc.id;
         dispatch(addNewEmptyNote(newNote));
         dispatch(setActiveNote(newNote));
+       
 
+    }
+}
 
+export const startLoadingNotes = () => {
+    return async(dispatch,getState) => {
+
+        const { uid } = getState().auth;
+
+        if(!uid) throw new Error('El UID del usuario no existe');
+       
+        const notes = await loadNotes(uid);
+        dispatch(setNotes(notes));
     }
 }
