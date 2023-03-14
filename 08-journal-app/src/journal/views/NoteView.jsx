a882287e644624fa56802f69e4f2088ebc6ee028
Raul Romero <link_2642@hotmail.com>
2023-03-13 22:08:38 -0600

    update: actualizacion de las entradas

--- 08-journal-app/src/journal/views/NoteView.jsx
+++ 08-journal-app/src/journal/views/NoteView.jsx
@@ -1,13 +1,18 @@
+import { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveNote, startSaveNote } from '../../store/journal';
+
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
+
 import { useForm } from '../../hooks/useForm';
+
 import { ImageGallery } from '../components'
 
 
 export const NoteView = () => {
 
+    const dispatch = useDispatch(  )
     const { active:note } = useSelector( state => state.journal );
 
     const { body, title , date, onInputChange, formState } =  useForm(note)
@@ -16,6 +21,14 @@ export const NoteView = () => {
         const newDate = new Date(date);
         return newDate.toUTCString();
     }, [date])
+
+    useEffect(() => {
+      dispatch(setActiveNote(formState))
+    }, [formState])
+    
+    const onSaveNote = () => {
+        dispatch(startSaveNote());
+    }
 
   return (
     <Grid 
@@ -30,7 +43,9 @@ export const NoteView = () => {
             <Typography fontSize={ 39 } fontWeight='light' >{dateString}</Typography>
         </Grid>
         <Grid item>
-            <Button color="primary" sx={{ padding: 2 }}>
+            <Button 
+                onClick={onSaveNote}
+                color="primary" sx={{ padding: 2 }}>
                 <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                 Guardar
             </Button>
--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,7 +1,7 @@
 import { collection, doc, setDoc } from 'firebase/firestore/lite';
 import { FirebaseDB } from '../../firebase/config';
 import { loadNotes } from '../../helpers/loadNotes';
-import { addNewEmptyNote, setActiveNote,savingNewNote,setNotes } from './journalSlice';
+import { addNewEmptyNote, setActiveNote,savingNewNote,setNotes, setSaving, updateNote } from './journalSlice';
 
 export const startNewNote = () => {
     return async (dispatch,getState) => {
@@ -38,5 +38,23 @@ export const startLoadingNotes = () => {
        
         const notes = await loadNotes(uid);
         dispatch(setNotes(notes));
+    }
+}
+
+export const startSaveNote = () => {
+    return async(dispatch, getState) => {
+
+        dispatch(setSaving());
+
+        const { uid } = getState().auth;
+        const { active:note } = getState().journal;
+        const noteToFireStore = {...note};
+
+        delete noteToFireStore.id;
+        delete noteToFireStore.imageUrls; // si no bota error firestore
+        const docRef = doc(FirebaseDB,`${uid}/journal/notes/${note.id}`);
+        await setDoc(docRef,noteToFireStore,{merge: true})
+
+        dispatch(updateNote(note));
     }
 }
