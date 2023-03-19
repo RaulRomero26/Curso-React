467589fc2610955ff42bf885b3dfc3c68d1716e9
Raul Romero <link_2642@hotmail.com>
2023-03-18 18:27:42 -0600

    update: fin pruebas en el auth slice

--- 08-journal-app/test/store/auth/authSlice.test.js
+++ 08-journal-app/test/store/auth/authSlice.test.js
@@ -1,5 +1,5 @@
-import { authSlice } from "../../../src/store/auth/authSlice"
-import { initialState } from "../../fixtures/authFixtures";
+import { authSlice, login, logout,checkingCredentials } from "../../../src/store/auth/authSlice"
+import { demoUser, initialState,authenticatedState } from "../../fixtures/authFixtures";
 
 
 describe('Pruebas en el auth Slice', () => { 
@@ -12,5 +12,54 @@ describe('Pruebas en el auth Slice', () => {
 
     })
     
-    
+    test('debe de realizar la autenticacion', () => { 
+       
+        const state = authSlice.reducer(initialState, login(demoUser));
+        expect(state).toEqual({
+            status: 'authenticated', // 'checking', 'not-authenticated', 'authenticated'
+            uid: demoUser.uid,
+            email: demoUser.email,
+            displayName: demoUser.displayName,
+            photoURL: demoUser.photoURL,
+            errorMessage: null,
+        })
+     })
+
+    test('debe de realizar el logout', () => { 
+
+        const state = authSlice.reducer(authenticatedState, logout());
+        expect(state).toEqual({
+            status: 'not-authenticated', // 'checking', 'not-authenticated', 'authenticated'
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: undefined,
+        })
+        
+    })
+
+    test('debe de realizar el logout y mostrar un mensaje de error', () => { 
+
+        const errorMessage = 'Credenciales no son correctas'
+
+        const state = authSlice.reducer(authenticatedState, logout({errorMessage}));
+        expect(state).toEqual({
+            status: 'not-authenticated', // 'checking', 'not-authenticated', 'authenticated'
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: errorMessage,
+        })
+
+    })
+
+    test('debe de cambiar el estado a cheking', () => { 
+        
+        const state = authSlice.reducer(authenticatedState,checkingCredentials());
+        expect(state.status).toBe('checking')
+     })
+
+
  })
