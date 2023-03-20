7fbbab546e37f416220c86c62f51b9cfe972ae2e
Raul Romero <link_2642@hotmail.com>
2023-03-20 11:31:12 -0600

    update: localstorage last view

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -5,6 +5,7 @@ import { addHours } from 'date-fns'
 
 import { localizer,getMessagesES } from '../../helpers'
 import { CalendarEvent, Navbar } from "../"
+import { useState } from 'react'
 
 
 const events = [{
@@ -19,7 +20,30 @@ const events = [{
 	}
 }]
 
+
+
 export const CalendarPage = () => {
+	
+
+	const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
+
+
+	const onDoubleClick = (event) => {
+		console.log({ doubleClick: event });
+	
+	}
+	const onSelect = (event) => {
+		console.log({ click: event });
+	
+	}
+	const onViewChanged = (event) => {
+		console.log({ onViewChanged: event });
+		localStorage.setItem('lastView',event);
+		setLastView(event)
+	
+	}
+	
 
 	const eventStyleGetter = (event,start,end, isSelected) => {
 
@@ -44,6 +68,7 @@ export const CalendarPage = () => {
 				culture='es'
 				localizer={localizer}
 				events={events}
+				defaultView={lastView}
 				startAccessor="start"
 				endAccessor="end"
 				style={{ height: 'calc(100vh - 80px)' }}
@@ -52,6 +77,9 @@ export const CalendarPage = () => {
 				components={{
 					event: CalendarEvent
 				}}
+				onDoubleClickEvent={onDoubleClick}
+				onSelectEvent={onSelect}
+				onView={onViewChanged}
 			/>
 		</>
 	)
