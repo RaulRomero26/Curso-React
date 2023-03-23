59e7d4cc62a8f2f38878a0b76b0af42757ed632d
RaulRomero26 <link_2642@hotmail.com>
2023-03-23 14:33:06 -0600

    update: primera configuracion de rutas

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,15 @@
-console.log('hola mundo')
+const express = require('express');
+require('dotenv').config();
+
+//crear el servidor de express
+const app = express();
+
+//rutas
+app.use('/api/auth', require('./routes/auth'));
+
+//directorio publico
+app.use(express.static('public'));
+//escuchar
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
