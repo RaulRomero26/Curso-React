03b68023be194d16544bf4a111a07dfccd571dbf
Raul Romero <link_2642@hotmail.com>
2023-03-26 08:23:18 -0600

    enponts eventos

--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -4,22 +4,20 @@ const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('.
 const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
+//si quieres validar todos los endpoint con un middleware,
+router.use(validarJWT)
 //todas validacion del jwt
 //obtener eventos
-router.get('/', 
-[ validarJWT ], 
+router.get('/',  
 getEventos)
 //crear evneto
 router.post('/', 
-[ validarJWT ],
 crearEvento)
 //actualizar evneto
 router.put('/:id', 
-[ validarJWT ],
 actualizarEvento)
 //borrar evento
 router.delete('/:id',
-[ validarJWT ],
 eliminarEvento)
 
 module.exports = router;
