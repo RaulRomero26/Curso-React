9151b478a1bacd605694b76db9f48727cea25814
Raul Romero <link_2642@hotmail.com>
2023-03-26 08:07:51 -0600

    update: inicio endponts crud

---
+++ 10-calendar-backend/controllers/events.js
@@ -0,0 +1,37 @@
+const { response } = require('express')
+
+const getEventos = (req,res = response) => {
+
+    return res.status(200).json({
+        ok: true,
+        msg: 'get evnetos'
+    })
+}
+const crearEvento = (req,res = response) => {
+
+    return res.status(200).json({
+        ok: true,
+        msg: 'crear evento'
+    })
+}
+const actualizarEvento = (req,res = response) => {
+
+    return res.status(200).json({
+        ok: true,
+        msg: 'actualizar evento'
+    })
+}
+const eliminarEvento = (req,res = response) => {
+
+    return res.status(200).json({
+        ok: true,
+        msg: 'eliminar evnento'
+    })
+}
+
+module.exports = {
+    getEventos,
+    crearEvento,
+    actualizarEvento,
+    eliminarEvento
+}
--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -1,11 +1,25 @@
 
+const {Router} = require('express');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const router = Router();
 
 //todas validacion del jwt
 //obtener eventos
-router.get('/', getEventos)
+router.get('/', 
+[ validarJWT ], 
+getEventos)
 //crear evneto
-router.post('/', crearEvento)
+router.post('/', 
+[ validarJWT ],
+crearEvento)
 //actualizar evneto
-router.put('/:id', actualizarEvento)
+router.put('/:id', 
+[ validarJWT ],
+actualizarEvento)
 //borrar evento
-router.delete('/:id',eliminarEvento)
+router.delete('/:id',
+[ validarJWT ],
+eliminarEvento)
+
+module.exports = router;
