e862eb44360c17bd9ffb57f739bf9426fca7f0f1
Raul Romero <link_2642@hotmail.com>
2023-03-26 10:56:28 -0600

    update: eliminacion de eventos

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -82,12 +82,43 @@ const actualizarEvento = async (req,res = response) => {
    
 }
 
-const eliminarEvento = (req,res = response) => {
+const eliminarEvento = async(req,res = response) => {
 
-    return res.status(200).json({
-        ok: true,
-        msg: 'eliminar evnento'
-    })
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if(!evento){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese ID'
+            });
+        }
+
+        if(evento.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de eliminar este evento'
+            });
+        }
+    
+        const eventoEliminado = await Evento.findByIdAndDelete(eventoId);
+
+        return res.status(200).json({
+            ok: true,
+            evento: eventoEliminado
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+        
+    }
 }
 
 module.exports = {
