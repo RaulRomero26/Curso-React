a9539b9c69547d2f2f404fb874e3b4cab259b9d1
RaulRomero26 <link_2642@hotmail.com>
2023-03-30 11:48:31 -0600

    update: inicio pruebas en UiStore

---
+++ 10-calendar/test/hooks/useUiStore.test.js
@@ -0,0 +1,37 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { renderHook } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { useUiStore } from '../../src/hooks/useUiStore';
+import { store, uiSlice } from '../../src/store';
+
+const getMockStore = ( initialState ) => {
+    return configureStore({
+        reducer: {
+            ui: uiSlice.reducer
+        },
+        preloadedState: {
+            ui: {...initialState}
+        }
+    })
+}
+
+
+describe('Pruebas en useStore', () => {
+
+    const mockStore = getMockStore({ isDateModalOpen: false })
+    
+    test('debe de regresar los valores por defecto', () => {
+
+        const { result } = renderHook( () => useUiStore(), {
+            wrapper: ({children}) => <Provider store={mockStore}>{children}</Provider>
+        });
+      
+        expect(result.current).toEqual({
+            isDateModalOpen: false,
+            closeDateModal: expect.any(Function),
+            openDateModal: expect.any(Function),
+            toggleDateModal: expect.any(Function),
+        })
+    })
+    
+})
