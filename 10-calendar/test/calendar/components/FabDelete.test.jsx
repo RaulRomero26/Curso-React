981ff62d21902d6a0571491ac4ad10a54ab404ad
RaulRomero26 <link_2642@hotmail.com>
2023-03-30 11:32:31 -0600

    update: inicio pruebas FabDelete

---
+++ 10-calendar/test/calendar/components/FabDelete.test.jsx
@@ -0,0 +1,21 @@
+import { render,screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { FabDelete } from '../../../src/calendar/components/FabDelete';
+import { store } from '../../../src/store';
+
+
+describe('Pruebas em <FabDelete />', () => { 
+
+    test('debe de mostrar el componente correctamente', () => {
+      
+        render(
+            <Provider store={ store }>
+                <FabDelete/>
+            </Provider>
+        );
+        screen.debug()
+
+    })
+    
+
+})
